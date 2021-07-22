3434f23b0d69afd014d1b90edb215b8dea272624
hejinzhi <504151911@qq.com>
2021-07-22 17:21:24 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('hey');
+  res.end('hello world');
 }).listen(process.env.PORT || 3000, () => {
   console.log('App listening on port 3000');
 });
